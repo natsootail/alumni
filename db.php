b70442434ad4200d7e71369cae55f01eb45a21fe
natsootail <45388104+natsootail@users.noreply.github.com>
2018-11-27 15:43:32 -0800

    Update db.php

--- db.php
+++ db.php
@@ -1,4 +1,4 @@
 <?php
 
 DATE_DEFAULT_TIMEZONE_SET('ASIA/MANILA');
-$db = new mysqli('localhost','root','','alumni_tracing');
+$db = new mysqli('sql12.freemysqlhosting.net','sql12267374','FJ5Z7FssAj','sql12267374');
